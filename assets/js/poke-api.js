068dc96841dab49d0c8b2f86d2881eeedcc29e3c
leonseto <lhseto@outlook.com>
2023-07-31 22:27:07 -0300

    Troca de imagens dos Pokémons (artwork oficial)

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -12,7 +12,7 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     pokemon.types = types
     pokemon.type = type
 
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+    pokemon.photo = pokeDetail.sprites.other['official-artwork'].front_default // ".official-artwork" não dava certo
 
     return pokemon
 }
